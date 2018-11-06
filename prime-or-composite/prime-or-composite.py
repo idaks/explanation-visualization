bffc12d8e6c2667c18b12b55fcdd5de00fd99381
Bertram Ludaescher <ludaesch@illinois.edu>
2018-11-05 20:24:49 -0600

    swapped rows and columns, so remainder < divisor is obvious

--- prime-or-composite/prime-or-composite.py
+++ prime-or-composite/prime-or-composite.py
@@ -8,20 +8,20 @@ N = int(sys.argv[1])                                      # number N to test
 
 d = 2
 is_prime = True
-while d*d <= N:                                           # the largest divisor d to try is ceiling(sqrt(N))
+while d*d <= N:                                           # enough to test only d <= sqrt(N)
     q, r = divmod(N,d)
-    print("[ {0} = {1} * {2} + {3} ]".format(N, d, q, r)) # Number = divisor * quotient + remainder
-    for i in range(d):                                    # d is the number of columns to create
-        print("{0:3}".format(i+1), q * "o")               # .. each having q many elements 'o' 
+    print("[ {0} = {1} * {2} + {3} ]".format(N, d, q, r)) # Number = Divisor * Quotient + Remainder
+    for i in range(q):                                    # q is the number of rows to create
+        print("{0:3}".format(i+1), d * "o")               # .. each being d elements (columns) wide
     if r == 0:                                            # No remainder?
         is_prime = False
-        print("PROVED composite: {0} = {1} * {2}".format(N, d, q)) # .. we got a composite!
+        print("PROVED composite: {0} = {1} * {2}".format(N, d, q)) # .. we got a composite for sure!
     else:
-        print("   ", r * "*")                                     # .. for now: display remainder.
+        print("   ", r * "*")                                      # .. for now: display remainder
     print()
     d = d + 1
 
 if is_prime:
-    print("PROVED", N, "is PRIME!")
+    print("PROVED", N, "is PRIME!")                        # Only now can we be sure!
 else:
-    print("Sorry", N, "is composite ..")
+    print("Sorry", N, "is composite ..")                   # .. ok, we knew this already (redundant)
