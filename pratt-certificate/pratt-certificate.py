31c6f8999fa7a498c3b906dd61aeb508ad7bcc33
Bertram Ludaescher <ludaesch@illinois.edu>
2018-12-08 13:01:40 -0600

    improved factorization use

--- pratt-certificate/pratt-certificate.py
+++ pratt-certificate/pratt-certificate.py
@@ -1,18 +1,6 @@
 #!/usr/bin/env python3
 
-def factors(n):
-    """Return sorted set of prime factors of n"""
-    d = 2
-    factors = set() 
-    while n > 1:
-      if n % d == 0:
-        factors.add(d)
-        n = n/d
-      else:
-        d = d + 1
-    return sorted(factors) # sort to avoid out-of-order presentation
-
-def factorization(n):
+def factorize(n):
     """Return prime factorization of n as sorted list"""
     d = 2
     factors = []
@@ -33,27 +21,29 @@ def lucas(n):
         print("\nLEAF (PRIME : WITNESS) = (2:1)")
         return 
     print("\n--- TESTING {0} ".format(n) + 70*"-")
+
+    factorization = factorize(n-1)   
+    factor_count = {}
+    for p in factorization:
+        factor_count[p] = factor_count.get(p,0) + 1
+
     for a in range(2,n):
         if pow(a, n-1, n) != 1:
             print("{0} is COMPOSITE because {1}^{2} != 1 (mod {0})".format(n,a,n-1))
             break
         else:
-            obligations = factorization(n-1)
-            for q in obligations:
+            for (q,m) in factor_count.items():  # lower prime q with multiplicity m 
                 e = (n-1)//q
                 if pow(a, e, n) == 1:
                     print("// {0} is NO witness since {0}^({1}/{2}) = {0}^{3} == 1 (mod {4})".format(a,n-1,q,e,n))
                     break
                 else:
-                    print(".. EDGE: {0}^({1}/{2}) = {0}^{3} != 1 (mod {4})".format(a,n-1,q,e,n))
+                    for i in range(m): # print multiple (i.e., m) copies of the edge 
+                        print(".. EDGE: {0}^({1}/{2}) = {0}^{3} != 1 (mod {4})".format(a,n-1,q,e,n))
             else:
                 print(".. VERTEX: {1}^{2} == 1 (mod {0})".format(n,a,n-1))
-                print(".. PRODUCT: {0} = {1} + 1".format(n, "*".join(str(q) for q in obligations)))
+                print(".. PRODUCT: {0} = {1} + 1".format(n, "*".join(str(q) for q in factorization)))
                 print("=> (PRIME : WITNESS) = ({0} : {1})".format(n,a))
-                # Call obligations recursively here? Or better return to the caller .. !?
-                # for q in sorted(set(obligations), reverse=True):
-                #     print(80 * "-")
-                #     lucas(q)
                 break # use this break to quit after first witness 
     return
 
